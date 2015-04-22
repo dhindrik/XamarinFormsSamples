8db6d6ddc25ca816ec3860d366e5f1fa4e98804e
Daniel Hindrikes <daniel@hindrikes.se>
2015-04-22 12:04:23 +0200

    sample code

---
+++ TitleView/TitleViewSample/TitleViewSample.iOS/ExtendedContentPageRenderer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using TitleViewSample.iOS;
+using TitleViewSample;
+
+[assembly: ExportRenderer(typeof(MainView), typeof(ExtendedContentPageRenderer))]
+namespace TitleViewSample.iOS
+{
+    public class ExtendedContentPageRenderer : PageRenderer
+    {
+        public ExtendedContentPageRenderer()
+        {
+           
+        }
+
+
+       public override void WillMoveToParentViewController(UIViewController parent)
+       {
+           base.WillMoveToParentViewController(parent);
+
+           var page = (ExtendedContentPage)Element;
+
+           var renderer = RendererFactory.GetRenderer(page.TitleView);
+           var view = renderer.NativeView;
+           view.SizeToFit();
+
+           parent.NavigationItem.TitleView = view.Subviews[0];
+       }
+        
+    }
+}
